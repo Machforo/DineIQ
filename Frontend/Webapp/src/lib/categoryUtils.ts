200b266f955ded618ee5f9d0022792b189abd9e9
MalhotraUmang <umangmalhotra1980@gmail.com>
2026-02-24 10:38:21 +0530

    menu images restored

--- Frontend/Webapp/src/lib/categoryUtils.ts
+++ Frontend/Webapp/src/lib/categoryUtils.ts
@@ -1,139 +1,155 @@
 import { Category, MenuItem } from './data';
 
-const MENU_IMAGE_BASE = '/src/assets/menu-images';
+/**
+ * Eagerly load all menu images from src/assets/menu-images.
+ * This is the most robust way in Vite to handle dynamic assets while keeping paths relative.
+ */
+const menuImageAssets = import.meta.glob('../assets/menu-images/**/*.{jpeg,jpg,png,webp,svg}', {
+  eager: true,
+  import: 'default',
+}) as Record<string, string>;
+
+/**
+ * Helper to resolve a path relative to 'src/assets/menu-images'
+ */
+function resolveMenuAsset(relativePath: string): string {
+  // The keys in menuImageAssets are relative to this file: '../assets/menu-images/...'
+  const key = `../assets/menu-images/${relativePath}`;
+  return menuImageAssets[key] || '';
+}
 
 // Map of category names to local representative images
 const CATEGORY_IMAGES: Record<string, string> = {
-  'FROM THE GARDEN': `${MENU_IMAGE_BASE}/from-the-garden/Summer-Salad.jpeg`,
-  'SOUP': `${MENU_IMAGE_BASE}/soup/Bone-Broth-&-Mushroom-Soup.jpeg`,
-  'STARTERS FROM THE SEA': `${MENU_IMAGE_BASE}/starters-from-the-sea/Torched-Salmon.jpeg`,
-  'STARTERS FROM THE LAND': `${MENU_IMAGE_BASE}/starters-from-the-land/Dry-Rub-Chicken-Wings.jpeg`,
-  'STEAMED BAO BUNS': `${MENU_IMAGE_BASE}/steamed-bao-buns/Paneer-Bao.jpeg`,
-  'BURGERS & SANDWICHES': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Giant-Burger.jpeg`,
-  'PASTA': `${MENU_IMAGE_BASE}/pasta/Spaghetti-Carbonara.jpeg`,
-  'MAINS FROM THE LAND': `${MENU_IMAGE_BASE}/mains-from-the-land/Ash-Roasted-Chicken-Espetada.jpeg`,
-  'MAINS FROM THE SEA': `${MENU_IMAGE_BASE}/mains-from-the-sea/Catch-of-the-Day.jpeg`,
-  'GRILLS': `${MENU_IMAGE_BASE}/grills/TOMAHAWK-STEAK.jpeg`,
-  'SIDES': `${MENU_IMAGE_BASE}/sides/Garlic,-Paprika,-Cumin-&-Coriander-Fries.jpeg`,
-  "CHILDREN'S MENU": `${MENU_IMAGE_BASE}/childrens-menu/BREADED-CHICKEN-BREAST.jpeg`,
-  'DESSERTS': `${MENU_IMAGE_BASE}/desserts/Basque-Cheesecake.jpeg`,
-  "CHEF SPECIAL": `${MENU_IMAGE_BASE}/recommendations/chef-special.jpeg`,
-  'BESTSELLER': `${MENU_IMAGE_BASE}/recommendations/bestseller.jpeg`,
+  'FROM THE GARDEN': resolveMenuAsset('from-the-garden/Summer-Salad.jpeg'),
+  'SOUP': resolveMenuAsset('soup/Bone-Broth-&-Mushroom-Soup.jpeg'),
+  'STARTERS FROM THE SEA': resolveMenuAsset('starters-from-the-sea/Torched-Salmon.jpeg'),
+  'STARTERS FROM THE LAND': resolveMenuAsset('starters-from-the-land/Dry-Rub-Chicken-Wings.jpeg'),
+  'STEAMED BAO BUNS': resolveMenuAsset('steamed-bao-buns/Paneer-Bao.jpeg'),
+  'BURGERS & SANDWICHES': resolveMenuAsset('burgers-and-sandwiches/Giant-Burger.jpeg'),
+  'PASTA': resolveMenuAsset('pasta/Spaghetti-Carbonara.jpeg'),
+  'MAINS FROM THE LAND': resolveMenuAsset('mains-from-the-land/Ash-Roasted-Chicken-Espetada.jpeg'),
+  'MAINS FROM THE SEA': resolveMenuAsset('mains-from-the-sea/Catch-of-the-Day.jpeg'),
+  'GRILLS': resolveMenuAsset('grills/TOMAHAWK-STEAK.jpeg'),
+  'SIDES': resolveMenuAsset('sides/Garlic,-Paprika,-Cumin-&-Coriander-Fries.jpeg'),
+  "CHILDREN'S MENU": resolveMenuAsset('childrens-menu/BREADED-CHICKEN-BREAST.jpeg'),
+  'DESSERTS': resolveMenuAsset('desserts/Basque-Cheesecake.jpeg'),
+  "CHEF SPECIAL": resolveMenuAsset('recommendations/chef-special.jpeg'),
+  'BESTSELLER': resolveMenuAsset('recommendations/bestseller.jpeg'),
 };
 
 // Comprehensive map of specific menu items to local images
 const ITEM_IMAGES: Record<string, string> = {
   // FROM THE GARDEN
-  'AGED STEAK & BRIE': `${MENU_IMAGE_BASE}/from-the-garden/Aged-Steak-&-Brie-Salad.jpeg`,
-  'BURRATA SALAD': `${MENU_IMAGE_BASE}/from-the-garden/Burrata-Salad.jpeg`,
-  'CALIFORNIA SALAD BOWL': `${MENU_IMAGE_BASE}/from-the-garden/California-Salad-Bowl.jpeg`,
-  'CHICKEN CAESAR': `${MENU_IMAGE_BASE}/from-the-garden/Chicken-Caesar.jpeg`,
-  'HARVEST SALAD': `${MENU_IMAGE_BASE}/from-the-garden/Harvest-Salad.jpeg`,
-  'SEARED TUNA SALAD': `${MENU_IMAGE_BASE}/from-the-garden/Seared-Tuna-Salad.jpeg`,
-  'SUMMER SALAD': `${MENU_IMAGE_BASE}/from-the-garden/Summer-Salad.jpeg`,
+  'AGED STEAK & BRIE': resolveMenuAsset('from-the-garden/Aged-Steak-&-Brie-Salad.jpeg'),
+  'BURRATA SALAD': resolveMenuAsset('from-the-garden/Burrata-Salad.jpeg'),
+  'CALIFORNIA SALAD BOWL': resolveMenuAsset('from-the-garden/California-Salad-Bowl.jpeg'),
+  'CHICKEN CAESAR': resolveMenuAsset('from-the-garden/Chicken-Caesar.jpeg'),
+  'HARVEST SALAD': resolveMenuAsset('from-the-garden/Harvest-Salad.jpeg'),
+  'SEARED TUNA SALAD': resolveMenuAsset('from-the-garden/Seared-Tuna-Salad.jpeg'),
+  'SUMMER SALAD': resolveMenuAsset('from-the-garden/Summer-Salad.jpeg'),
 
   // SOUP
-  'BONE BROTH & MUSHROOM SOUP': `${MENU_IMAGE_BASE}/soup/Bone-Broth-&-Mushroom-Soup.jpeg`,
-  'GREEN SOUP': `${MENU_IMAGE_BASE}/soup/Green-Soup.jpeg`,
-  'MULLIGATAWNY SOUP': `${MENU_IMAGE_BASE}/soup/Mulligatawny-Soup.jpeg`,
-  'TUSCAN WHITE BEAN': `${MENU_IMAGE_BASE}/soup/Tuscan-White-Bean.jpeg`,
+  'BONE BROTH & MUSHROOM SOUP': resolveMenuAsset('soup/Bone-Broth-&-Mushroom-Soup.jpeg'),
+  'GREEN SOUP': resolveMenuAsset('soup/Green-Soup.jpeg'),
+  'MULLIGATAWNY SOUP': resolveMenuAsset('soup/Mulligatawny-Soup.jpeg'),
+  'TUSCAN WHITE BEAN': resolveMenuAsset('soup/Tuscan-White-Bean.jpeg'),
 
   // STARTERS FROM THE SEA
-  'FISH CEVICHE SALAD': `${MENU_IMAGE_BASE}/starters-from-the-sea/Fish-Ceviche-Salad.jpeg`,
-  'GINGER & SESAME SALMON': `${MENU_IMAGE_BASE}/starters-from-the-sea/Ginger-&-Sesame-Salmon.jpeg`,
-  'TORCHED SALMON': `${MENU_IMAGE_BASE}/starters-from-the-sea/Torched-Salmon.jpeg`,
+  'FISH CEVICHE SALAD': resolveMenuAsset('starters-from-the-sea/Fish-Ceviche-Salad.jpeg'),
+  'GINGER & SESAME SALMON': resolveMenuAsset('starters-from-the-sea/Ginger-&-Sesame-Salmon.jpeg'),
+  'TORCHED SALMON': resolveMenuAsset('starters-from-the-sea/Torched-Salmon.jpeg'),
 
   // STARTERS FROM THE LAND
-  'CRISPY FRIED CAULIFLOWER': `${MENU_IMAGE_BASE}/starters-from-the-land/Crispy-Fried-Cauliflower.jpeg`,
-  'DRY-RUB CHICKEN WINGS': `${MENU_IMAGE_BASE}/starters-from-the-land/Dry-Rub-Chicken-Wings.jpeg`,
-  'MUSHROOM CEVICHE': `${MENU_IMAGE_BASE}/starters-from-the-land/Mushroom-Ceviche.jpeg`,
-  'SPINACH & MUSHROOM WRAP': `${MENU_IMAGE_BASE}/starters-from-the-land/Spinach-&-Mushroom-Wrap.jpeg`,
+  'CRISPY FRIED CAULIFLOWER': resolveMenuAsset('starters-from-the-land/Crispy-Fried-Cauliflower.jpeg'),
+  'DRY-RUB CHICKEN WINGS': resolveMenuAsset('starters-from-the-land/Dry-Rub-Chicken-Wings.jpeg'),
+  'MUSHROOM CEVICHE': resolveMenuAsset('starters-from-the-land/Mushroom-Ceviche.jpeg'),
+  'SPINACH & MUSHROOM WRAP': resolveMenuAsset('starters-from-the-land/Spinach-&-Mushroom-Wrap.jpeg'),
 
   // STEAMED BAO BUNS
-  'PANEER BAO': `${MENU_IMAGE_BASE}/steamed-bao-buns/Paneer-Bao.jpeg`,
-  'PRESSED PORK BELLY': `${MENU_IMAGE_BASE}/steamed-bao-buns/Pressed-Pork-Belly.jpeg`,
-  'SLOW-COOKED BEEF BRISKET': `${MENU_IMAGE_BASE}/steamed-bao-buns/Slow-Cooked-Beef-Brisket.jpeg`,
+  'PANEER BAO': resolveMenuAsset('steamed-bao-buns/Paneer-Bao.jpeg'),
+  'PRESSED PORK BELLY': resolveMenuAsset('steamed-bao-buns/Pressed-Pork-Belly.jpeg'),
+  'SLOW-COOKED BEEF BRISKET': resolveMenuAsset('steamed-bao-buns/Slow-Cooked-Beef-Brisket.jpeg'),
 
   // BURGERS & SANDWICHES
-  'CHICKEN & GOAT CHEESE QUESADILLA': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Chicken-&-Goat-Cheese-Quesadilla.jpeg`,
-  'CHICKEN KEBAB SANDWICH': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Chicken-Kebab-Sandwich.jpeg`,
-  'CUBANO SANDWICH': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Cubano-Sandwich.jpeg`,
-  'GIANT BURGER': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Giant-Burger.jpeg`,
-  'GRILLED TOFU & VEGGIE SANDWICH': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Grilled-Tofu-&-Veggie-Sandwich.jpeg`,
-  'HARVEST BRISKET BURGER': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Harvest-Brisket-Burger.jpeg`,
-  'HARVEST CLUB SANDWICH': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Harvest-Club-Sandwich.jpeg`,
-  'KOREAN FRIED MUSHROOM TACOS': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Korean-Fried-Mushroom-Tacos.jpeg`,
-  'PICANHA STEAK SANDWICH': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Picanha-Steak-Sandwich.jpeg`,
-  'PROSCIUTTO OPEN SANDWICH': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Prosciutto-Open-Sandwich.jpeg`,
-  'PULLED PORK & CARAMELIZED ONION': `${MENU_IMAGE_BASE}/burgers-and-sandwiches/Pulled-Pork-&-Caramelized-Onion.jpeg`,
+  'CHICKEN & GOAT CHEESE QUESADILLA': resolveMenuAsset('burgers-and-sandwiches/Chicken-&-Goat-Cheese-Quesadilla.jpeg'),
+  'CHICKEN KEBAB SANDWICH': resolveMenuAsset('burgers-and-sandwiches/Chicken-Kebab-Sandwich.jpeg'),
+  'CUBANO SANDWICH': resolveMenuAsset('burgers-and-sandwiches/Cubano-Sandwich.jpeg'),
+  'GIANT BURGER': resolveMenuAsset('burgers-and-sandwiches/Giant-Burger.jpeg'),
+  'GRILLED TOFU & VEGGIE SANDWICH': resolveMenuAsset('burgers-and-sandwiches/Grilled-Tofu-&-Veggie-Sandwich.jpeg'),
+  'HARVEST BRISKET BURGER': resolveMenuAsset('burgers-and-sandwiches/Harvest-Brisket-Burger.jpeg'),
+  'HARVEST CLUB SANDWICH': resolveMenuAsset('burgers-and-sandwiches/Harvest-Club-Sandwich.jpeg'),
+  'KOREAN FRIED MUSHROOM TACOS': resolveMenuAsset('burgers-and-sandwiches/Korean-Fried-Mushroom-Tacos.jpeg'),
+  'PICANHA STEAK SANDWICH': resolveMenuAsset('burgers-and-sandwiches/Picanha-Steak-Sandwich.jpeg'),
+  'PROSCIUTTO OPEN SANDWICH': resolveMenuAsset('burgers-and-sandwiches/Prosciutto-Open-Sandwich.jpeg'),
+  'PULLED PORK & CARAMELIZED ONION': resolveMenuAsset('burgers-and-sandwiches/Pulled-Pork-&-Caramelized-Onion.jpeg'),
 
   // PASTA
-  'BACON RIGATONI': `${MENU_IMAGE_BASE}/pasta/Bacon-Rigatoni.jpeg`,
-  'BEEF CARBONARA': `${MENU_IMAGE_BASE}/pasta/Beef-Carbonara.jpeg`,
-  'BUTTERNUT & SAGE RAVIOLI': `${MENU_IMAGE_BASE}/pasta/Butternut-&-Sage-Ravioli.jpeg`,
-  'SPAGHETTI CARBONARA': `${MENU_IMAGE_BASE}/pasta/Spaghetti-Carbonara.jpeg`,
-  'TRADIZIONALE': `${MENU_IMAGE_BASE}/pasta/Tradizionale.jpeg`,
+  'BACON RIGATONI': resolveMenuAsset('pasta/Bacon-Rigatoni.jpeg'),
+  'BEEF CARBONARA': resolveMenuAsset('pasta/Beef-Carbonara.jpeg'),
+  'BUTTERNUT & SAGE RAVIOLI': resolveMenuAsset('pasta/Butternut-&-Sage-Ravioli.jpeg'),
+  'SPAGHETTI CARBONARA': resolveMenuAsset('pasta/Spaghetti-Carbonara.jpeg'),
+  'TRADIZIONALE': resolveMenuAsset('pasta/Tradizionale.jpeg'),
 
   // MAINS FROM THE LAND
-  'ASH-ROASTED CHICKEN ESPETADA': `${MENU_IMAGE_BASE}/mains-from-the-land/Ash-Roasted-Chicken-Espetada.jpeg`,
-  'CHIMICHURRI CHARRED CAULIFLOWER STEAK': `${MENU_IMAGE_BASE}/mains-from-the-land/Chimichurri-Charred-Cauliflower-Steak.jpeg`,
-  'GLAZED LAMB STEAKS': `${MENU_IMAGE_BASE}/mains-from-the-land/Glazed-Lamb-Steaks.jpeg`,
-  'HERB CRUSTED LAMB SHANK': `${MENU_IMAGE_BASE}/mains-from-the-land/Herb-Crusted-Lamb-Shank.jpeg`,
-  'OSTRICH WELLINGTON': `${MENU_IMAGE_BASE}/mains-from-the-land/Ostrich-Wellington.jpeg`,
-  'PERSIAN LAMB KEBAB': `${MENU_IMAGE_BASE}/mains-from-the-land/Persian-Lamb-Kebab.jpeg`,
-  'ROASTED PORK BELLY': `${MENU_IMAGE_BASE}/mains-from-the-land/Roasted-Pork-Belly.jpeg`,
-  'SMOKED BEEF BRISKET': `${MENU_IMAGE_BASE}/mains-from-the-land/Smoked-Beef-Brisket.jpeg`,
-  'VEGETABLE KEBAB': `${MENU_IMAGE_BASE}/mains-from-the-land/Vegetable-Kebab.jpeg`,
-  'WHOLE ROAST BABY CHICKEN': `${MENU_IMAGE_BASE}/mains-from-the-land/Whole-Roast-Baby-Chicken.jpeg`,
-  'ZUCCHINI INVOLTINI': `${MENU_IMAGE_BASE}/mains-from-the-land/Zucchini-Involtini.jpeg`,
+  'ASH-ROASTED CHICKEN ESPETADA': resolveMenuAsset('mains-from-the-land/Ash-Roasted-Chicken-Espetada.jpeg'),
+  'CHIMICHURRI CHARRED CAULIFLOWER STEAK': resolveMenuAsset('mains-from-the-land/Chimichurri-Charred-Cauliflower-Steak.jpeg'),
+  'GLAZED LAMB STEAKS': resolveMenuAsset('mains-from-the-land/Glazed-Lamb-Steaks.jpeg'),
+  'HERB CRUSTED LAMB SHANK': resolveMenuAsset('mains-from-the-land/Herb-Crusted-Lamb-Shank.jpeg'),
+  'OSTRICH WELLINGTON': resolveMenuAsset('mains-from-the-land/Ostrich-Wellington.jpeg'),
+  'PERSIAN LAMB KEBAB': resolveMenuAsset('mains-from-the-land/Persian-Lamb-Kebab.jpeg'),
+  'ROASTED PORK BELLY': resolveMenuAsset('mains-from-the-land/Roasted-Pork-Belly.jpeg'),
+  'SMOKED BEEF BRISKET': resolveMenuAsset('mains-from-the-land/Smoked-Beef-Brisket.jpeg'),
+  'VEGETABLE KEBAB': resolveMenuAsset('mains-from-the-land/Vegetable-Kebab.jpeg'),
+  'WHOLE ROAST BABY CHICKEN': resolveMenuAsset('mains-from-the-land/Whole-Roast-Baby-Chicken.jpeg'),
+  'ZUCCHINI INVOLTINI': resolveMenuAsset('mains-from-the-land/Zucchini-Involtini.jpeg'),
 
   // MAINS FROM THE SEA
-  'CATCH OF THE DAY': `${MENU_IMAGE_BASE}/mains-from-the-sea/Catch-of-the-Day.jpeg`,
-  'FISH + CHIPS': `${MENU_IMAGE_BASE}/mains-from-the-sea/Fish-+-Chips.jpeg`,
-  'FRITTO MISTO': `${MENU_IMAGE_BASE}/mains-from-the-sea/Fritto-Misto.jpeg`,
-  'OLIVE OIL ROASTED SALMON': `${MENU_IMAGE_BASE}/mains-from-the-sea/Olive-Oil-Roasted-Salmon.jpeg`,
+  'CATCH OF THE DAY': resolveMenuAsset('mains-from-the-sea/Catch-of-the-Day.jpeg'),
+  'FISH + CHIPS': resolveMenuAsset('mains-from-the-sea/Fish-+-Chips.jpeg'),
+  'FRITTO MISTO': resolveMenuAsset('mains-from-the-sea/Fritto-Misto.jpeg'),
+  'OLIVE OIL ROASTED SALMON': resolveMenuAsset('mains-from-the-sea/Olive-Oil-Roasted-Salmon.jpeg'),
 
   // GRILLS
-  'DRY AGED PORK CHOPS': `${MENU_IMAGE_BASE}/grills/DRY-AGED-PORK-CHOPS.jpeg`,
-  'FARMER MAX\' CHICKEN': `${MENU_IMAGE_BASE}/grills/Farmer-Max\'-Chicken.jpeg`,
-  'GRILLED JUMBO PRAWNS': `${MENU_IMAGE_BASE}/grills/GRILLED-JUMBO-PRAWNS.jpeg`,
-  'GRILLED RIB RACK': `${MENU_IMAGE_BASE}/grills/GRILLED-RIB-RACK.jpeg`,
-  'MEAT LOVERS PLATTER': `${MENU_IMAGE_BASE}/grills/MEAT-LOVERS-PLATTER.jpeg`,
-  'MIXED SEAFOOD PLATTER': `${MENU_IMAGE_BASE}/grills/MIXED-SEAFOOD-PLATTER.jpeg`,
+  'DRY AGED PORK CHOPS': resolveMenuAsset('grills/DRY-AGED-PORK-CHOPS.jpeg'),
+  'FARMER MAX\' CHICKEN': resolveMenuAsset('grills/Farmer-Max\'-Chicken.jpeg'),
+  'GRILLED JUMBO PRAWNS': resolveMenuAsset('grills/GRILLED-JUMBO-PRAWNS.jpeg'),
+  'GRILLED RIB RACK': resolveMenuAsset('grills/GRILLED-RIB-RACK.jpeg'),
+  'MEAT LOVERS PLATTER': resolveMenuAsset('grills/MEAT-LOVERS-PLATTER.jpeg'),
+  'MIXED SEAFOOD PLATTER': resolveMenuAsset('grills/MIXED-SEAFOOD-PLATTER.jpeg'),
   // Note: NEW YORK STEAK has 2 variants — handled by nameEncounterCounter in getMenuItemImage
-  'RIBEYE': `${MENU_IMAGE_BASE}/grills/RIBEYE.jpeg`,
-  'SURF & TURF': `${MENU_IMAGE_BASE}/grills/SURF-&-TURF.jpeg`,
-  'T-BONE STEAK': `${MENU_IMAGE_BASE}/grills/T-BONE-STEAK.jpeg`,
-  'TOMAHAWK STEAK': `${MENU_IMAGE_BASE}/grills/TOMAHAWK-STEAK.jpeg`,
+  'RIBEYE': resolveMenuAsset('grills/RIBEYE.jpeg'),
+  'SURF & TURF': resolveMenuAsset('grills/SURF-&-TURF.jpeg'),
+  'T-BONE STEAK': resolveMenuAsset('grills/T-BONE-STEAK.jpeg'),
+  'TOMAHAWK STEAK': resolveMenuAsset('grills/TOMAHAWK-STEAK.jpeg'),
 
   // SIDES
-  'GARLIC, PAPRIKA, CUMIN & CORIANDER FRIES': `${MENU_IMAGE_BASE}/sides/Garlic,-Paprika,-Cumin-&-Coriander-Fries.jpeg`,
-  'OREGANO, ROSEMARY, GARLIC, CHEESE & ONION FRIES': `${MENU_IMAGE_BASE}/sides/Oregano,-Rosemary,-Garlic,-Cheese-&-Onion-Fries.jpeg`,
-  'SPICY DEHYDRATED PICKLED FRIES': `${MENU_IMAGE_BASE}/sides/Spicy-Dehydrated-Pickled-Fries.jpeg`,
+  'GARLIC, PAPRIKA, CUMIN & CORIANDER FRIES': resolveMenuAsset('sides/Garlic,-Paprika,-Cumin-&-Coriander-Fries.jpeg'),
+  'OREGANO, ROSEMARY, GARLIC, CHEESE & ONION FRIES': resolveMenuAsset('sides/Oregano,-Rosemary,-Garlic,-Cheese-&-Onion-Fries.jpeg'),
+  'SPICY DEHYDRATED PICKLED FRIES': resolveMenuAsset('sides/Spicy-Dehydrated-Pickled-Fries.jpeg'),
 
   // CHILDREN'S MENU
-  'BREADED CHICKEN BREAST': `${MENU_IMAGE_BASE}/childrens-menu/BREADED-CHICKEN-BREAST.jpeg`,
-  'GRILLED FISH FILLET': `${MENU_IMAGE_BASE}/childrens-menu/GRILLED-FISH-FILLET.jpeg`,
-  'PAN FRIED BEEF PATTY': `${MENU_IMAGE_BASE}/childrens-menu/PAN-FRIED-BEEF-PATTY.jpeg`,
-  'POTATO GNOCCHI': `${MENU_IMAGE_BASE}/childrens-menu/POTATO-GNOCCHI.jpeg`,
-  'STIR FRIED NOODLES': `${MENU_IMAGE_BASE}/childrens-menu/STIR-FRIED-NOODLES.jpeg`,
-  'VANILLA ICE CREAM': `${MENU_IMAGE_BASE}/childrens-menu/VANILLA-ICE-CREAM.jpeg`,
+  'BREADED CHICKEN BREAST': resolveMenuAsset('childrens-menu/BREADED-CHICKEN-BREAST.jpeg'),
+  'GRILLED FISH FILLET': resolveMenuAsset('childrens-menu/GRILLED-FISH-FILLET.jpeg'),
+  'PAN FRIED BEEF PATTY': resolveMenuAsset('childrens-menu/PAN-FRIED-BEEF-PATTY.jpeg'),
+  'POTATO GNOCCHI': resolveMenuAsset('childrens-menu/POTATO-GNOCCHI.jpeg'),
+  'STIR FRIED NOODLES': resolveMenuAsset('childrens-menu/STIR-FRIED-NOODLES.jpeg'),
+  'VANILLA ICE CREAM': resolveMenuAsset('childrens-menu/VANILLA-ICE-CREAM.jpeg'),
 
   // DESSERTS
-  'BASQUE CHEESECAKE': `${MENU_IMAGE_BASE}/desserts/Basque-Cheesecake.jpeg`,
-  'CHOCOLATE OVERLOAD': `${MENU_IMAGE_BASE}/desserts/Chocolate-Overload.jpeg`,
-  'CYCLIC CHURROS': `${MENU_IMAGE_BASE}/desserts/Cyclic-Churros.jpeg`,
-  'FRESH FRUIT SALAD': `${MENU_IMAGE_BASE}/desserts/Fresh-Fruit-Salad.jpeg`,
-  'ICE CREAM OF THE DAY': `${MENU_IMAGE_BASE}/desserts/Ice-Cream-of-the-Day.jpeg`,
-  'LAVENDER PARFAIT': `${MENU_IMAGE_BASE}/desserts/Lavender-Parfait.jpeg`,
-  'SORBET OF THE DAY': `${MENU_IMAGE_BASE}/desserts/Sorbet-of-the-Day.jpeg`,
-  'VANILLA MILLEFEUILLE': `${MENU_IMAGE_BASE}/desserts/Vanilla-Millefeuille.jpeg`,
+  'BASQUE CHEESECAKE': resolveMenuAsset('desserts/Basque-Cheesecake.jpeg'),
+  'CHOCOLATE OVERLOAD': resolveMenuAsset('desserts/Chocolate-Overload.jpeg'),
+  'CYCLIC CHURROS': resolveMenuAsset('desserts/Cyclic-Churros.jpeg'),
+  'FRESH FRUIT SALAD': resolveMenuAsset('desserts/Fresh-Fruit-Salad.jpeg'),
+  'ICE CREAM OF THE DAY': resolveMenuAsset('desserts/Ice-Cream-of-the-Day.jpeg'),
+  'LAVENDER PARFAIT': resolveMenuAsset('desserts/Lavender-Parfait.jpeg'),
+  'SORBET OF THE DAY': resolveMenuAsset('desserts/Sorbet-of-the-Day.jpeg'),
+  'VANILLA MILLEFEUILLE': resolveMenuAsset('desserts/Vanilla-Millefeuille.jpeg'),
 };
 
 // Two variants for 'New York Steak' — alternated per render cycle
 const NEW_YORK_STEAK_IMAGES = [
-  `${MENU_IMAGE_BASE}/grills/NEW-YORK-STEAK-1.jpeg`,
-  `${MENU_IMAGE_BASE}/grills/NEW-YORK-STEAK-2.jpeg`,
+  resolveMenuAsset('grills/NEW-YORK-STEAK-1.jpeg'),
+  resolveMenuAsset('grills/NEW-YORK-STEAK-2.jpeg'),
 ];
 
 // Tracks how many times a specific item name has been resolved this session
